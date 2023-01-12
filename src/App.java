d3baf083c7d4eac64f32de66ef4784b4cf9f598c
Flavia <flavia.bulad@gmail.com>
2023-01-12 15:14:43 -0300

    add solution

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        List<String> frutas1 = new ArrayList<String>();
+
+        frutas1.add("Morango");
+        frutas1.add("Uva");
+        frutas1.add("Acerola");
+        frutas1.add("Manga");
+     
+        List<String> frutas2 = new ArrayList<String>();
+
+        frutas2.add("Pêra");
+        frutas2.add("Caju");
+        frutas2.add("Morango");
+        frutas2.add("Kiwi");
+
+        for (String fruta : frutas1) {
+            if (frutas2.contains(fruta)) {
+                System.out.println("Fruta repetida: " + fruta);
+            }
+        }
+
     }
 }
